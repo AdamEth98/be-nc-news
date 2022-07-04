7d9c8eafdd5a5eef61625aa29dc5816b3862afaf
AdamEth98 <adam.etherington1@outlook.com>
2022-07-04 12:16:01 +0100

    if accepted, adds the GET /api/topics endpoint with tests

--- __tests__/get/api-topics.test.js
+++ __tests__/get/api-topics.test.js
@@ -9,36 +9,38 @@ beforeAll(() => seed(testData));
 afterAll(() => db.end());
 
 describe("GET /api/topics", () => {
-  it("should respond with a status code of 200", () => {
-    return request(app).get("/api/topics").expect(200);
-  });
-  it("should respond with an object containing an array", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then(({ body }) => {
-        expect(Array.isArray(body.topics)).toBe(true);
-      });
-  });
-  it("array should be of length 3", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then(({ body }) => {
-        expect(body.topics.length).toBe(3);
-      });
-  });
-  it("each topic should be an object with two keys, slug & description, both of which are strings", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then(({ body }) => {
-        body.topics.forEach((topic) => {
-          expect(topic).toEqual({
-            slug: expect.any(String),
-            description: expect.any(String),
+  describe("api calls", () => {
+    it("should respond with a status code of 200", () => {
+      return request(app).get("/api/topics").expect(200);
+    });
+    it("should respond with an object containing an array", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body }) => {
+          expect(Array.isArray(body.topics)).toBe(true);
+        });
+    });
+    it("array should be of length 3", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.topics.length).toBe(3);
+        });
+    });
+    it("each topic should be an object with two keys, slug & description, both of which are strings", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body }) => {
+          body.topics.forEach((topic) => {
+            expect(topic).toEqual({
+              slug: expect.any(String),
+              description: expect.any(String),
+            });
           });
         });
-      });
+    });
   });
 });
--- app.js
+++ app.js
@@ -7,4 +7,9 @@ app.use(express.json());
 // GET ROUTES
 app.get("/api/topics", getTopics);
 
+// 404
+app.get("/*", (req, res) => {
+  res.status(404).send({ status: 404, msg: `Error: endpoint (${req.path}) not found.` });
+});
+
 module.exports = app;
