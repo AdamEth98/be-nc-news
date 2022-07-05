3ade47c852c496042f0bfd694b7078d9c240df58
AdamEth98 <adam.etherington1@outlook.com>
2022-07-05 12:32:02 +0100

    adds a test for a specific value

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -98,40 +98,55 @@ describe("GET endpoints", () => {
     });
   });
   describe("GET /api/users", () => {
-    it("should return a status code of 200", () => {
-      return request(app).get("/api/users").expect(200);
-    });
-    it("should return an object with a key of users which is an array", () => {
-      return request(app)
-        .get("/api/users")
-        .expect(200)
-        .then(({ body }) => {
-          expect(body).toHaveProperty("users");
-          expect(Array.isArray(body.users)).toBe(true);
-        });
-    });
-    it("should return all objects from the users table", () => {
-      return request(app)
-        .get("/api/users")
-        .expect(200)
-        .then(({ body }) => {
-          expect(body.users.length).toBe(4);
-        });
-    });
-    it("should return all objects full populated with the correct keys and value types", () => {
-      return request(app)
-        .get("/api/users")
-        .expect(200)
-        .then(({ body }) => {
-          expect(body.users.length > 0).toBe(true);
-          body.users.forEach((user) => {
-            expect(user).toEqual({
-              username: expect.any(String),
-              name: expect.any(String),
-              avatar_url: expect.any(String),
+    describe("api calls", () => {
+      it("should return a status code of 200", () => {
+        return request(app).get("/api/users").expect(200);
+      });
+      it("should return an object with a key of users which is an array", () => {
+        return request(app)
+          .get("/api/users")
+          .expect(200)
+          .then(({ body }) => {
+            expect(body).toHaveProperty("users");
+            expect(Array.isArray(body.users)).toBe(true);
+          });
+      });
+      it("should return all objects from the users table", () => {
+        return request(app)
+          .get("/api/users")
+          .expect(200)
+          .then(({ body }) => {
+            expect(body.users.length).toBe(4);
+          });
+      });
+      it("should return all objects fully populated with the correct keys and value types", () => {
+        return request(app)
+          .get("/api/users")
+          .expect(200)
+          .then(({ body }) => {
+            expect(body.users.length > 0).toBe(true);
+            body.users.forEach((user) => {
+              expect(user).toEqual({
+                username: expect.any(String),
+                name: expect.any(String),
+                avatar_url: expect.any(String),
+              });
             });
           });
-        });
+      });
+      it("should return all objects fully populated with the correct keys and values", () => {
+        return request(app)
+          .get("/api/users")
+          .expect(200)
+          .then(({ body }) => {
+            expect(body.users.length).toBe(4);
+            expect(body.users[0]).toEqual({
+              username: "butter_bridge",
+              name: "jonny",
+              avatar_url: "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+            });
+          });
+      });
     });
   });
 });
